f63af423732123b49a918b2b8b1096e4571f615e
Kristina <abrkriss@gmail.com>
2022-11-25 15:08:47 +0300

    Zadacha_1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,15 @@
+﻿void Zadacha25()
+{
+    //Используя определение степени числа, 
+    //напишите цикл, который принимает на вход два натуральных числа (A и B) и 
+    //возводит число A в степень B.
+
+    Console.WriteLine("Введите число x");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите число y");
+    int y = Convert.ToInt32(Console.ReadLine());
+    
+    Console.WriteLine($"{x} ^ {y}" + "\t = " + Math.Pow(x, y));
+}
+Zadacha25();
