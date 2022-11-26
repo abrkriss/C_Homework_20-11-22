123dafb14f19fbdd9f043eb0f9f773932e879755
Kristina <abrkriss@gmail.com>
2022-11-26 11:28:20 +0300

    Zadacha_3_11

--- Zadacha_3/Program.cs
+++ Zadacha_3/Program.cs
@@ -1,29 +1,33 @@
-﻿int[] nums = { 0, 8, -2, 4, 6, 2, 1, 7 };
+﻿void Zadacha29()
+{
+    int[] nums = { 0, 8, -2, 4, 6, 2, 1, 7 };
 
 
-int temp;
-if (nums.Length == 8)
-{
-for (int i = 0; i < nums.Length - 1; i++)
-{
-    for (int n = i + 1; n < nums.Length; n++)
+    int temp;
+    if (nums.Length == 8)
     {
-        if (nums[i] > nums[n])
+        for (int i = 0; i < nums.Length - 1; i++)
         {
-            temp = nums[i];
-            nums[i] = nums[n];
-            nums[n] = temp;
+            for (int n = i + 1; n < nums.Length; n++)
+            {
+                if (nums[i] > nums[n])
+                {
+                    temp = nums[i];
+                    nums[i] = nums[n];
+                    nums[n] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("Вывод отсортированного массива");
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine(nums[i]);
         }
     }
+    else
+    {
+        Console.WriteLine("Массив меньше 8 значений");
+    }
 }
-
-Console.WriteLine("Вывод отсортированного массива");
-for (int i = 0; i < nums.Length; i++)
-{
-    Console.WriteLine(nums[i]);
-}
-}
-else
-{
-    Console.WriteLine("Массив меньше 8 значений");
-}
+Zadacha29();
