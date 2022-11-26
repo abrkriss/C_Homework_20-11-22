a7560a0bfefbfa0d7688e3c0792c6a620cd3bf6b
Kristina <abrkriss@gmail.com>
2022-11-26 11:26:20 +0300

    Zadacha_3_1

--- Zadacha_3/Program.cs
+++ Zadacha_3/Program.cs
@@ -1,7 +1,9 @@
-﻿int[] nums = { 0, 8, -2, 4, 6 };
+﻿int[] nums = { 0, 8, -2, 4, 6, 2, 1, 7 };
 
 
 int temp;
+if (nums.Length == 8)
+{
 for (int i = 0; i < nums.Length - 1; i++)
 {
     for (int n = i + 1; n < nums.Length; n++)
@@ -19,4 +21,9 @@ Console.WriteLine("Вывод отсортированного массива");
 for (int i = 0; i < nums.Length; i++)
 {
     Console.WriteLine(nums[i]);
+}
+}
+else
+{
+    Console.WriteLine("Массив меньше 8 значений");
 }
